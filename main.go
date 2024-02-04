c489bb0edbd51e1852ecb641cf875625df000c3e
mursalbekov1 <mursalbekov.merey@gmail.com>
2024-02-04 18:33:50 +0600

    docs update

--- main.go
+++ main.go
@@ -3,6 +3,6 @@ package main
 import dBot "discord-bot/bot"
 
 func main() {
-	dBot.BotToken = "MTIwMjUxNTM5NDc3NTA4OTI0Mg.GqX_dF.DabF1kwKCSvSyolz2J5hEazaKnpQZmhWar9IBs"
+	dBot.BotToken = "MTIwMjUxNTM5NDc3NTA4OTI0Mg.G7x0uB.aeOb6i1Ymh0AttA3z1oe7sVlWSZ0Ermkqoe888"
 	dBot.Run()
 }
